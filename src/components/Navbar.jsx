74ba7296a2c6444daddd1b7dccd27cea3f1e02dd
shaxzod22 <shaxzod01901@gmail.com>
2024-05-07 19:22:06 +0500

    fixing some bugs

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,38 +6,46 @@ import { Context } from '../App';
 const Navbar = () => {
   const {pathname} = useLocation()
   const [setHasBurger] = useContext(Context)
- 
+  
   return (
-    <div className='flex flex-col xl:flex-row text-[18px] items-center gap-[20px] xl:gap-[32px]'>
-    <Link to={'/'} onClick={()=>{
+    <ul className='flex flex-col xl:flex-row text-[18px] items-center gap-[20px] xl:gap-[32px]'>
+    <li onClick={()=>{
       setHasBurger(false)
       document.body.style.overflow ='auto'
-    }} className={` flex w-full justify-between items-center px-4 xl:px-0 mb-[3px]`}>
+    }} className='w-full'>
+    <Link to={'/'} className={` flex w-full justify-between items-center px-4 xl:px-0 mb-[3px]`}>
     <span className={`${pathname== '/'?'font-medium':''}`}>Menyu</span>
     <IoMdCheckmark className={`xl:hidden text-[#51267D] w-6 h-6 ${pathname =='/'?'block':'hidden'}`} />
     </Link> 
-    <Link to={'/branches'} onClick={()=>{
+    </li>
+    <li className='w-full' onClick={()=>{
       setHasBurger(false)
       document.body.style.overflow ='auto'
-    }} className={` flex w-full justify-between items-center px-4 xl:px-0 mb-[3px]`}>
+    }}>
+    <Link to={'/branches'} className={` flex w-full justify-between items-center px-4 xl:px-0 mb-[3px]`}>
     <span className={`${pathname.includes('/branches')?'font-medium':''}`}>Filiallar</span>
     <IoMdCheckmark className={`xl:hidden text-[#51267D] w-6 h-6 ${pathname.includes('/branches')?'block':'hidden'}`} />
     </Link> 
-    <Link to={'/about'} onClick={()=>{
+    </li>
+    <li onClick={()=>{
       setHasBurger(false)
       document.body.style.overflow ='auto'
-    }} className={` flex w-full justify-between items-center px-4 xl:px-0 mb-[3px]`}>
+    }} className='w-full'>
+    <Link to={'/about'} className={` flex w-full justify-between items-center px-4 xl:px-0 mb-[3px]`}>
     <span className={`${pathname.includes('/about')?'font-medium':''} whitespace-nowrap`}>Biz haqimizda</span>
     <IoMdCheckmark className={`xl:hidden text-[#51267D] w-6 h-6 ${pathname.includes('/about')?'block':'hidden'}`} />
-    </Link> 
-    <Link to={'/contact'} onClick={()=>{
+    </Link>
+    </li>
+     
+    <li onClick={()=>{
       setHasBurger(false)
       document.body.style.overflow ='auto'
-    }} className={` flex w-full justify-between items-center px-4 xl:px-0 mb-[3px]`}>
+    }} className='w-full'>
+      <Link to={'/contact'} className={` flex w-full justify-between items-center px-4 xl:px-0 mb-[3px]`}>
     <span className={`${pathname.includes('/contact')?'font-medium':''}`}>Bog'lanish</span>
     <IoMdCheckmark className={`xl:hidden text-[#51267D] w-6 h-6 ${pathname.includes('/contact')?'block':'hidden'}`} />
-    </Link> 
-    </div>
+    </Link> </li>
+    </ul>
     )
   }
   
